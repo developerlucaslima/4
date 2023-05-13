9c30d465d24611c94756667d7406df3f3c1d1013
JoviTech <developerlucaslima@gmail.com>
2023-05-13 04:23:44 -0300

    stitches:  theme and globalStyles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,25 @@
 import { styled } from "../styles"
 
 const Button = styled('button', {
-  backgroundColor: "$rocketseat",
+  backgroundColor: '$green300',
   borderRadius: 4,
   border: 0,
   padding: '4px 8px',
 
   span: {
-    fontWeight: 'bold',
+    fontWeight: 'bold'
   },
 
   '&:hover': {
-    filter: 'brighness(0.8)'
-  },
+    filter: 'brightness(0.8)'
+  }
 })
 
 export default function Home() {
   return (
     <Button>
-      <h1>
-        <span>Send </span>
-        E-mail 
-      </h1>
+      <span>Testedassd</span>
+      Enviar
     </Button>
   )
 }
