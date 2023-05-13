32102dea0d5dcae48f9cad02d24e61f2c4bb2304
JoviTech <developerlucaslima@gmail.com>
2023-05-12 23:48:43 -0300

    test stitches: first style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: "$rocketseat"
+})
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Button>
+      <h1>Send</h1>
+    </Button>
   )
 }
