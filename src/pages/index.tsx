f36c2bcfdcf6e83c107ccd1901b1ae3a9c45317b
JoviTech <developerlucaslima@gmail.com>
2023-05-12 23:54:28 -0300

    stitches: anothers tests

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
 import { styled } from "../styles"
 
 const Button = styled('button', {
-  backgroundColor: "$rocketseat"
+  backgroundColor: "$rocketseat",
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brighness(0.8)'
+  },
 })
 
 export default function Home() {
   return (
     <Button>
-      <h1>Send</h1>
+      <h1>
+        <span>Send </span>
+        E-mail 
+      </h1>
     </Button>
   )
 }
