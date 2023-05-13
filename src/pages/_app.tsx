a3bf916d5699146467f4ee932da974e3d0a78731
JoviTech <developerlucaslima@gmail.com>
2023-05-13 04:45:23 -0300

    header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
 import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
 
-globalStyles()
+import logoImg from "../assets/logo.svg" 
+import { Conainter, Header } from "../styles/pages/app"
 
+globalStyles()
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <Conainter>
+      <Header>
+        <img src={logoImg.src} alt="Logo Ignite" />
+      </Header>
+      <Component {...pageProps} />
+    </Conainter>
+  ) 
 }
 
 export default App
---
+++ src/styles/pages/app.ts
@@ -0,0 +1,15 @@
+import { styled } from "..";
+
+export const Conainter = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  minHeight: '100vh',
+})
+
+export const Header = styled('div', {
+  padding: '2rem 0',
+  width: '100%',
+  maxWidth: 1100,
+  margin: '0 auto',
+})
